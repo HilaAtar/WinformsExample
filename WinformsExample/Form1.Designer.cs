2a4e38a204cc83ed532ed5fb27e2f137866fe65c
student <student@A26-13>
2022-03-21 14:25:22 +0200

    Add project files.

---
+++ WinformsExample/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace WinformsExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.add = new System.Windows.Forms.Button();
+            this.numA = new System.Windows.Forms.TextBox();
+            this.numB = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.recv = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(481, 113);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 0;
+            this.add.Text = "ADD  A+B";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // numA
+            // 
+            this.numA.Location = new System.Drawing.Point(342, 86);
+            this.numA.Name = "numA";
+            this.numA.Size = new System.Drawing.Size(100, 20);
+            this.numA.TabIndex = 1;
+            // 
+            // numB
+            // 
+            this.numB.Location = new System.Drawing.Point(592, 86);
+            this.numB.Name = "numB";
+            this.numB.Size = new System.Drawing.Size(100, 20);
+            this.numB.TabIndex = 2;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(469, 33);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(100, 20);
+            this.result.TabIndex = 3;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(72, 113);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(151, 277);
+            this.treeView1.TabIndex = 4;
+            // 
+            // recv
+            // 
+            this.recv.Location = new System.Drawing.Point(342, 300);
+            this.recv.Name = "recv";
+            this.recv.Size = new System.Drawing.Size(350, 20);
+            this.recv.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.recv);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.numB);
+            this.Controls.Add(this.numA);
+            this.Controls.Add(this.add);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.TextBox numA;
+        private System.Windows.Forms.TextBox numB;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox recv;
+    }
+}
+
---
+++ WinformsExample/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformsExample
+{
+    public partial class Form1 : Form
+    {
+        TreeNode mainNode;
+        public Form1()
+        {
+            InitializeComponent();
+            mainNode = new TreeNode();
+            Thread thr = new Thread(new ThreadStart(this.Sever));
+            thr.Start();
+
+            numA.Text = "0";
+            numB.Text = "0";
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            int res = int.Parse(this.numA.Text) + int.Parse(this.numB.Text);
+            this.result.Text = res.ToString();
+            this.mainNode.Name = "mainNode";
+            this.mainNode.Text = "Main";
+            this.treeView1.Nodes.Add(mainNode);
+        }
+        private void InvokeUI(Action a)
+        {
+            this.BeginInvoke(new MethodInvoker(a));
+        }
+        private string ToString(byte[] bytes)
+        {
+            string response = string.Empty;
+
+            foreach (byte b in bytes)
+                response += (Char)b;
+
+            return response;
+        }
+        public void Sever()
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 9999);
+            server.Start();
+            TcpClient client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
+
+            NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
+
+           
+            while (client.Connected)  //while the client is connected, we look for incoming messages
+            {
+                byte[] msg = new byte[1024];     //the messages arrive as byte array
+                ns.Read(msg, 0, msg.Length);   //the same networkstream reads the message sent by the client
+                InvokeUI(() => {
+                    this.recv.Text = ToString(msg);
+                });
+               // this.recv.Text = ToString(msg);
+            }
+        }
+    }
+}
